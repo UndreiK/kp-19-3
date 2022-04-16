57952956057325fcee8ce33ac27a51561b706ab2
Kleyankin Andrei <raujo@mail.ru>
2022-04-16 19:33:01 +0300

    add grouplist

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 import Users from "./components/users"
 import api from "./api"
 
 function App() {
-  const [users, setUsers] = useState(api.users.fetchAll())
+  const [users, setUsers] = useState()
+
+  useEffect(() => {
+    api.users.fetchAll().then((data) => setUsers(data))
+  }, [])
 
   const handleDelete = (userId) => {
     setUsers(users.filter((user) => user._id !== userId))
@@ -23,11 +27,13 @@ function App() {
 
   return (
     <div>
-      <Users
-        users={users}
-        onDelete={handleDelete}
-        onToggleBookmark={handleToggleBookmark}
-      />
+      {users && (
+        <Users
+          users={users}
+          onDelete={handleDelete}
+          onToggleBookmark={handleToggleBookmark}
+        />
+      )}
     </div>
   )
 }
