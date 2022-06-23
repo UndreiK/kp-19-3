6c2187cb221d219f70e14e696aa28d14fcd1640f
Kleyankin Andrei <raujo@mail.ru>
2022-06-23 22:11:37 +0300

    fix edit-user-page

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,19 +1,25 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { useParams, Redirect } from "react-router-dom"
 import EditUserPage from "../components/page/editUserPage/editUserPage"
 import UserPage from "../components/page/userPage"
 import UsersListPage from "../components/page/usersListPage"
+import { useAuth } from "../hooks/useAuth"
 import UserProvider from "../hooks/useUsers"
 
 const Users = () => {
   const params = useParams()
+  const { currentUser } = useAuth()
   const { userId, edit } = params
   return (
     <div>
       <UserProvider>
         {userId ? (
           edit ? (
-            <EditUserPage />
+            userId === currentUser._id ? (
+              <EditUserPage />
+            ) : (
+              <Redirect to={`/users/${currentUser._id}/edit`} />
+            )
           ) : (
             <UserPage userId={userId} />
           )
